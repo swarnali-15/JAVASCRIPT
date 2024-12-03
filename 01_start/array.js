de38903ecd906f1c6473392aecbab708d657bf93
Swarnali Nandy <91903231+swarnali-15@users.noreply.github.com>
2024-12-03 15:54:22 +0000

    array

--- 01_start/array.js
+++ 01_start/array.js
@@ -1,3 +1,13 @@
 const myArr=[2, 5, 6, 9];
 
 console.log(myArr);
+
+// object literals
+
+const JsUser={
+    name: "Swarnali",
+    age: 22,
+    location: "Kolkata",
+}
+
+console.log(JsUser.age);
