f1feb1563ead2f3e3be9dd7bd1777f9d4b957f16
Swarnali Nandy <91903231+swarnali-15@users.noreply.github.com>
2024-12-04 17:19:40 +0000

    object

--- 01_start/array.js
+++ 01_start/array.js
@@ -1,6 +1,6 @@
 const myArr=[2, 5, 6, 9];
 
-console.log(myArr);
+//console.log(myArr);
 
 // object literals
 
@@ -10,4 +10,11 @@ const JsUser={
     location: "Kolkata",
 }
 
-console.log(JsUser.age);
+//console.log(JsUser.age);
+
+const obj1={1: "a", 2: "b"}
+const obj2={3:"a", 4:"b"}
+const obj4={5:"a", 6:"b"}
+
+const obj3=Object.assign({},obj1,obj2,obj4)
+console.log(obj3);
