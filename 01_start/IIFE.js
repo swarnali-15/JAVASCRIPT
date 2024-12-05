c0c880134ac5ec4d3f9f3c4585450b6e383070d6
Swarnali Nandy <91903231+swarnali-15@users.noreply.github.com>
2024-12-05 11:50:13 +0000

    iife

---
+++ 01_start/IIFE.js
@@ -0,0 +1,23 @@
+function con(){
+    console.log(`DB CONNECTED`);
+}
+
+con()
+// to remove the glabal polution we use iffe
+
+
+//-----------****************-------------
+
+
+
+// Javascript Execution Context
+//1. global exectuion context
+//2.function execution context
+//3.eval execution context
+
+//{} ->memory execution phase
+//    ->execution phase
+
+// global execution -> this
+// memory phase{}
+//execution phase
