70d21a0061c29ea6fac942bfa21df319d36d0ed5
Swarnali Nandy <91903231+swarnali-15@users.noreply.github.com>
2024-12-06 11:20:22 +0000

    truthy

---
+++ 01_start/truthy.js
@@ -0,0 +1,15 @@
+
+// Nullish Coalescing Operator(??): null undefined
+
+
+let val1;
+val1 = 5 ?? 10
+//console.log(val1);
+
+// Terniary Operator
+
+
+// condition ? true : false 
+
+const price =180
+price<=80 ? console.log("less than 80") : console.log ("more than 80")
